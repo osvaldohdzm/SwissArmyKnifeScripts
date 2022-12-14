868219925c03d4f69d8c164dc90d7aa7871609e2
Your Name <you@example.com>
2022-12-14 12:37:19 -0600

    saved

--- MergeCSV/merge-csv.ps1
+++ MergeCSV/merge-csv.ps1
@@ -1,3 +1,10 @@
+$output_path = "outputs"
+If(!(test-path -PathType container $output_path))
+{
+      New-Item -ItemType Directory -Path $output_path
+}
+
+
 Get-ChildItem *.csv -PipelineVariable File |
   ForEach-Object { Import-Csv $_ | Select *,@{l='FileName';e={$File.Name}}} |
-  Export-Csv output\output.csv -NoTypeInformation
+  Export-Csv $output_path\output.csv -NoTypeInformation
